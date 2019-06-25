912590d9c870b0fd0f0e136aab41c5c462ef116f
D-PC <D-PC@DESKTOP-MMJC98U>
2019-06-25 09:16:12 +0530

    Commit Third Session

---
+++ SeleniumSessionByNaveen/src/SeleniumBasic/Framehandling.java
@@ -0,0 +1,42 @@
+package SeleniumBasic;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Framehandling {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\D-PC\\Desktop\\selenium\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver(); //launch chrome
+		
+		driver.manage().window().maximize(); //maximize window
+		driver.manage().deleteAllCookies(); //delete all the cookies
+		
+		//dynamic wait
+		driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		
+		driver.get("https://classic.crmpro.com/login.cfm"); //enter URL
+		
+		driver.findElement(By.name("username")).sendKeys("brajesh2288");
+		driver.findElement(By.name("password")).sendKeys("brajesh221229");
+		driver.findElement(By.xpath("//input[@type='submit']")).click();
+		Thread.sleep(3000);
+		
+		driver.switchTo().frame("mainpanel");
+		Thread.sleep(2000);
+		
+		driver.findElement(By.xpath("//a[contains(text(),'Contacts')]")).click();	
+		System.out.println("Contact page is Clicked Now");
+		driver.close();
+		System.out.println("Driver is Closed Now");
+	}
+
+
+
+	}
+
+
